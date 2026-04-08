2cfb3627c715fc417b1b1317ade7b51f18f82eaf
ItsTuki <rar56721@gmail.com>
2026-04-08 10:46:48 -0400

    tablas para bdb

--- src/main/java/com/hospitalVM/atenciones/models/Medico.java
+++ src/main/java/com/hospitalVM/atenciones/models/Medico.java
@@ -1,8 +1,23 @@
 package com.hospitalVM.atenciones.models;
 
-import jakarta.persistence.Embeddable;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Table(name = "medicos")
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@ToString
 public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "medico_id")
+    private long medicoId;
+
     @Embeddable
     private Audit audit = new Audit();
 }
