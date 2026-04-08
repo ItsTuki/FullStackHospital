d1f174111cdb320d9943dfa7e68950e9fbb66786
ItsTuki <rar56721@gmail.com>
2026-04-08 11:05:28 -0400

    model complete

--- src/main/java/com/hospitalVM/atenciones/models/Medico.java
+++ src/main/java/com/hospitalVM/atenciones/models/Medico.java
@@ -1,6 +1,8 @@
 package com.hospitalVM.atenciones.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,6 +19,18 @@ public class Medico {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "medico_id")
     private long medicoId;
+
+    @Column(unique = true,nullable = false)
+    @NotBlank(message = "el campo no puede estar vacio bro")
+    @Pattern(regexp = "\\d(1,8)-[\\dKk]",message = "formato erroneo fomato xxxxxx-x")
+    private String run;
+
+    @Column(name ="nombre_completo",nullable = false )
+    @NotBlank(message = "no puedes dejar el campo vacio")
+    private String nombrecompleto;
+
+    @Column(name = "jefe_turno",nullable = false)
+    private Boolean jefeTurno;
 
     @Embeddable
     private Audit audit = new Audit();
