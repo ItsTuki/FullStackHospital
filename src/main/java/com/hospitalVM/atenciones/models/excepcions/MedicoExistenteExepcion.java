6c2ea345a51bc9ac42a5cc3f0d9e84f1f7b9cc61
ItsTuki <rar56721@gmail.com>
2026-04-08 12:09:48 -0400

    se modifica sercice y se le agregan exepciones

--- src/main/java/com/hospitalVM/atenciones/models/excepcions/MedicoExistenteExepcion.java
+++ src/main/java/com/hospitalVM/atenciones/models/excepcions/MedicoExistenteExepcion.java
@@ -1,7 +1,7 @@
 package com.hospitalVM.atenciones.models.excepcions;
 import com.hospitalVM.atenciones.repositories.MedicoRepository;
-public class MedicoExistenteExepcion extends RuntimeException{
-    public MedicoExistenteExepcion(String menssage){
+public class MedicoExistenteExepcion extends RuntimeException {
+    public MedicoExistenteExepcion(String menssage) {
         super(menssage);
     }
-
+}
